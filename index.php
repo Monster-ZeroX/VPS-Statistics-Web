eb0df639dc4fb0a1d06d6974acf9fd1b248ec5c9
Mⷨoͦns͛ᴛⷮeͤrͬ Zeͤrͬoͦ <69518719+Monster-ZeroX@users.noreply.github.com>
2021-08-29 19:50:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
 		<div class="row">
 				<div class="col">
 					<div align="center" class="card-header alert-dark">
-							&copy; Copyright <?php echo date("Y"); ?> <a href="https://github.com/hirunaofficial/VPS-Statistics-Web" target="_blank">VPS Statistics Web</a> | Created By <a href="http://hirunaofficial.github.io/" target="_blank">GD Hiruna</a>
+							&copy; Copyright <?php echo date("Y"); ?> <a href="https://t.me/FZBOTS" target="_blank">VPS Statistics Web</a>
 						</div>
 				</div>
 		</div>
